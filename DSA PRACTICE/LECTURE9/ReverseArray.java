b6b0166b6981513b2238e4b98e3486f8a05b267c
MANOJ KUMAR <109135089+BCAPATHSHALA@users.noreply.github.com>
2022-10-06 13:48:18 +0530

    Reverse an Array Using Swapping

---
+++ DSA PRACTICE/LECTURE9/ReverseArray.java
@@ -0,0 +1,54 @@
+// Reverse an Array Using Swapping
+import java.util.Scanner;
+
+class ReverseArray
+{
+	public static void reverseArray(int arr[],int size)
+	{
+		int start, end=size-1, temp=0;
+
+		for(start=0; start<size/2; start++)
+		{
+			temp=arr[start];
+			arr[start]=arr[end-start];
+			arr[end-start]=temp;
+		}
+	}
+
+	public static void printArray(int arr[],int size)
+	{
+		for(int i=0;i<size;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the size: ");
+		int size=sc.nextInt();
+
+		System.out.println("Input the elements");
+		int arr[]=new int[100];
+
+		for(int i=0;i<size;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+
+		reverseArray(arr,size);
+		printArray(arr,size);
+	}
+}
+
+/*
+OUTPUT:
+Enter the size: 4
+Input the elements
+6
+7
+8
+9
+9 7 8 6
+*/
